2556b557588b1a730ee690a1708758c95c77d64b
suzukikentarou <ken@light-groove.com>
2020-12-06 01:02:42 +0900

    First Commit

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+import math
+
+def fizzbuzz_convert(number):
+    if number%15==0:
+        return 'FizzBuzz'
+    if number%3==0:
+        return 'Fizz'
+    if number%5==0:
+        return 'Buzz'
+
+assert fizzbuzz_convert(3) == 'Fizz'
+assert fizzbuzz_convert(6) == 'Fizz'
+assert fizzbuzz_convert(5) == 'Buzz'
+assert fizzbuzz_convert(10) == 'Buzz'
+assert fizzbuzz_convert(15) == 'FizzBuzz'
+assert fizzbuzz_convert(30) == 'FizzBuzz'
